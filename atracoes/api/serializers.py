deb436625b6d25310d31a5ed5ae0091064b2b024
Gustavo Nogueira <gustavoasnogueira@gmail.com>
2019-08-15 08:52:54 -0300

    add api recursos

---
+++ atracoes/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from atracoes import models
+
+
+class AtracaoSerializer(ModelSerializer):
+
+    class Meta:
+        model = models.Atracao
+        fields = ('id', 'nome', 'descricao', 'horario_func', 'idade_min')
